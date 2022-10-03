6c750e197a7f9841df8b63c204317d422096cc70
Amod <amodnagpal46@gmail.com>
2022-10-03 18:21:13 +0530

    Runtime: 348 ms, faster than 28.17% of C++

---
+++ Minimum Time to Make Rope Colorful.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Problem link : https://leetcode.com/problems/minimum-time-to-make-rope-colorful/
+
+class Solution
+{
+public:
+    int minCost(string colors, vector<int> &neededTime)
+    {
+        long long ans = 0;
+        for (int i = 1; i < colors.size(); ++i)
+        {
+            int j = i - 1;
+            while (i < colors.size() && colors[i] == colors[j])
+            {
+                ans += min(neededTime[i], neededTime[j]);
+                if (neededTime[i] > neededTime[j])
+                    j = i;
+                i++;
+            }
+        }
+        return ans;
+    }
+};
