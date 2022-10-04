4c39b8c6d34a7967a105babc21da9961657a8c4b
Amod <amodnagpal46@gmail.com>
2022-10-04 11:48:24 +0530

    Runtime: 24 ms, faster than 30.13% of C++ online submissions

--- Path Sum.cpp
+++ Path Sum.cpp
@@ -3,10 +3,20 @@ using namespace std;
 
 // Problem link : https://leetcode.com/problems/path-sum/
 
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
 class Solution
 {
 public:
-    bool hasPathSum(Treenode *root, int targetSum)
+    bool hasPathSum(TreeNode *root, int targetSum)
     {
         if (root == NULL)
         {
