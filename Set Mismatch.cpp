949495c7534a772c114ebfc76282cf191e8452d7
Amod <amodnagpal46@gmail.com>
2022-10-23 19:45:05 +0530

    Runtime: 31 ms, faster than 95.07% of C++ online submissions

---
+++ Set Mismatch.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Problem link : https://leetcode.com/problems/set-mismatch/
+
+class Solution
+{
+public:
+    vector<int> findErrorNums(vector<int> &nums)
+    {
+        int len = nums.size(), dup, mis;
+        for (int index = 0; index < len; ++index)
+        {
+            if (nums[abs(nums[index]) - 1] < 0)
+                dup = abs(nums[index]);
+            else
+                nums[abs(nums[index]) - 1] *= -1;
+        }
+
+        for (int index = 0; index < len; ++index)
+        {
+            if (nums[index] > 0)
+            {
+                mis = index + 1;
+                break;
+            }
+        }
+        return {dup, mis};
+    }
+};
