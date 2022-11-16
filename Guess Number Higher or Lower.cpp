7b7fd74adfdf9272d6e7f0edf5551061ca66e2ab
Amod <amodnagpal46@gmail.com>
2022-11-16 09:19:27 +0530

    Runtime: 0 ms, faster than 100.00% of C++ online submissions

---
+++ Guess Number Higher or Lower.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Problem link : https://leetcode.com/problems/guess-number-higher-or-lower/
+
+int guess(int num)
+{
+    // return 0 1 or -1
+}
+
+class Solution
+{
+public:
+    int guessNumber(int n)
+    {
+        int low = 1;
+        int high = n;
+
+        while (low <= high)
+        {
+            int mid = low + (high - low) / 2;
+            int res = guess(mid);
+
+            if (res == 0)
+            {
+                return mid;
+            }
+
+            else if (res < 0)
+            {
+                high = mid - 1;
+            }
+
+            else
+            {
+                low = mid + 1;
+            }
+        }
+
+        return -1;
+    }
+};
