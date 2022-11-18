765a2dd22f685dda4cee864dbf32037860c86cfe
Amod <amodnagpal46@gmail.com>
2022-11-18 17:21:33 +0530

    Runtime: 3 ms, faster than 65.96% of C++ online submissions

---
+++ Ugly Number.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Problem link : https://leetcode.com/problems/ugly-number/
+
+class Solution
+{
+public:
+    bool isUgly(int n)
+    {
+        if (n > 0)
+        {
+            for (int i = 2; i < 6; i++)
+            {
+                while (n % i == 0)
+                {
+                    n = n / i;
+                }
+            }
+        }
+        return n == 1;
+    }
+};
