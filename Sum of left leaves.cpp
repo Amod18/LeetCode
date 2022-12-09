4e2ae54f26ac9d5742d12dc7be9c033e3123735d
Amod <amodnagpal46@gmail.com>
2022-12-09 19:02:05 +0530

    Runtime 8 ms, faster than 47.99% of online C++ submission

---
+++ Sum of left leaves.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Problem link :
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution
+{
+public:
+    void solve(TreeNode *root, int &res, int fac)
+    {
+        if (root != NULL)
+        {
+            if (root->left == NULL && root->right == NULL)
+            {
+                res += root->val * fac;
+            }
+            solve(root->left, res, 1);
+            solve(root->right, res, 0);
+        }
+    }
+
+    int sumOfLeftLeaves(TreeNode *root)
+    {
+        int res = 0;
+        if (root == NULL || (root->left == NULL && root->right == NULL))
+        {
+            return res;
+        }
+
+        solve(root, res, 1);
+        return res;
+    }
+};
