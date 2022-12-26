6defc0ac4871f58c43827385a525c0a2dce6c36d
Amod <amodnagpal46@gmail.com>
2022-12-26 11:24:00 +0530

    Runtime 70ms, faster than 89.76% C++ online submissions

---
+++ Jump_Game.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Problem link : https://leetcode.com/problems/jump-game/
+
+class Solution
+{
+public:
+    bool canJump(vector<int> &nums)
+    {
+        int max_jump_index = 0;
+
+        for (int i = 0; i < nums.size(); i++)
+        {
+            max_jump_index = max(nums[i] + i, max_jump_index);
+            if (max_jump_index < i + 1)
+            {
+                break;
+            }
+        }
+
+        return max_jump_index >= nums.size() - 1;
+    }
+};
